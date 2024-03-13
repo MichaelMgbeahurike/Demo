4b8fb18706431f121319e66e30eee6d335212804
Michael Mgbeahurike <mgbeahurikem@gmail.com>
2024-03-13 09:27:38 +0000

    fourth change

--- Program1.java
+++ Program1.java
@@ -1,6 +1,8 @@
 class Program1{
     public static void main(String [] a) {
         System.out.println("Hello People1");
+	System.out.println("Hello People2");
+	System.out.println("Hello People3");
     }
 }
 	
